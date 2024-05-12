c26c0d1c8a816a49edb7da1158a6964902b6699c
sabkat ahmed rafi <sabkatahmedrafi@gmail.com>
2024-05-13 04:15:54 +0600

    add job, get all job, apply job, get applied job routes are created

--- index.js
+++ index.js
@@ -1,22 +1,151 @@
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
-const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
 
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    methods: 'GET,PUT,PATCH,POST,DELETE',
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: "GET,PUT,PATCH,POST,DELETE",
     credentials: true,
-  }))
+  })
+);
 
-app.use(express.json())
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME
+const apiKey = process.env.CLOUDINARY_API_KEY;
+const apiSecret = process.env.CLOUDINARY_API_SECRET;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+cloudinary.config({
+  cloud_name: cloudName,
+  api_key: apiKey,
+  api_secret: apiSecret,
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const user = process.env.MONGODB_USER;
+const key = process.env.MONGODB_KEY;
+
+const uri = `mongodb+srv://${user}:${key}@backendtest.ldjqqhi.mongodb.net/?retryWrites=true&w=majority&appName=backendTest`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const database = client.db("CareerCove");
+    const user = database.collection("user"); 
+    const allJobs = database.collection("allJobs"); 
+    const appliedJobs = database.collection("appliedJobs"); 
+
+    // Showing user in the UI 
+    app.get("/user" , async(req, res) => {
+        const result = await user.find().toArray();
+        res.send(result);
+    })
+
+    // Showing all jobs in the UI
+    app.get("/allJobs", async (req, res) => {
+      const search = req.query.search
+      const query = { title: {$regex: search, $options: "i"}}
+        const result = await allJobs.find(query).toArray();
+        res.send(result);
+    })
+
+    // Showing Single job post in the UI
+    app.get("/allJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await allJobs.findOne(query);
+      res.send(result);
+    })
+
+    // Showing applied jobs in the UI
+    app.get("/appliedJobs", async (req, res) => {
+      const result = await appliedJobs.find().toArray();
+      res.send(result);
+    })
+
+    // Saving user info in the Database Using Cloudinary and Multer 
+    app.post("/user", upload.single("photo"), async (req, res) => {
+        try {
+          const { name, email } = req.body;
+          const photo = req.file.path;
+  
+          const result = await cloudinary.uploader.upload(photo);
+  
+          const photoUrl = result.secure_url;
+          const userResult = await user.insertOne({ name, email, photoUrl });
+          res.send(userResult);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error uploading image");
+        }
+      });
+
+      // Save job post to the database 
+      app.post("/allJobs", async (req, res) => {
+        const jobInfo = req.body
+        const result = await allJobs.insertOne(jobInfo)
+        res.send(result)
+      })
+
+      // Saving applied jobs on database 
+      app.post("/appliedJobs", async (req, res) => {
+        const {_id} = req.body.appliedJobsInfo
+        const jobId = _id
+        const updateApplicantResult = await allJobs.updateOne(
+          {_id: new ObjectId(jobId)},
+          {$inc: {applicantNumber: 1 }}
+        )
+        const {title, photo, description, salary, jobDeadline, jobOption, postDate, applyerName, applyerEmail, cv } = req.body.appliedJobsInfo 
+
+        const appliedJobsResult = await appliedJobs.insertOne({title, photo, description, salary, jobDeadline, jobOption, postDate, applyerName, applyerEmail, cv })
+        res.send()
+      })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World rafi!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
